2b5d155dc24f2277add2123b1574266d9c14d10e
Agung Nawawi <agung@fairtech.com.sg>
2023-07-27 20:49:07 +0700

    Section 2 - Customize Text View

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+            Text("Turtle Rock")
+                .font(.title)
+            .padding()
         }
-        .padding()
     }
 }
 
