d6221a1dd7c90e21e661e86c8027ed2c4b5e2e66
Agung Nawawi <agung@fairtech.com.sg>
2023-07-27 20:42:51 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Agung Nawawi on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
